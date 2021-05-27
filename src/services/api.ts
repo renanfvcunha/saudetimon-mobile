97e957769a72fb833f24cbf25a2ff5cc9db33231
Renan Cunha <rfvcunha@hotmail.com>
2021-05-27 16:40:49 -0300

    Changing api production route

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiUrl = __DEV__
   ? process.env.API_URL || 'http://10.0.2.2:3333'
-  : 'https://api.saudetimon24h.b2bdesenvolvimentos.app';
+  : 'https://api.saudetimon24h.com.br';
 
 const api = axios.create({
   baseURL: apiUrl,
